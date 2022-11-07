d7d4c88a3e0bec42c162b112a3834a4c0b55f9a2
Damon <DamonBaker27@hotmail.com>
2022-11-07 13:43:43 -0600

    minor changes to product show page

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,13 @@ Rails.application.routes.draw do
   post "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
   delete "products/remove_from_cart/:id", to: "products#remove_from_cart", as: "remove_from_cart"
 
+  # /checkout/create something
+  scope "/checkout" do
+    post "create", to: "checkout#create", as: "checkout_create"
+    get "success", to: "checkout#success", as: "checkout_success"
+    get "cancel", to: "checkout#cancel", as: "checkout_cancel"
+  end
+
   resources :products
   # get "search", to: "Products#search"
   resources :cases
