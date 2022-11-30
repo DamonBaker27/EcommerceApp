a0442fd3d499a80f5dd5c7a47e6cbbe36f3bc75a
Damon <DamonBaker27@hotmail.com>
2022-11-30 16:33:07 -0600

    updated checkout controller to have multiple line items

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -3,9 +3,47 @@ class CheckoutController < ApplicationController
     # establish a connection to Stripe and then redirect the user to the payment screen
     Stripe.api_key = "sk_test_51M71cXAa7W4k6Y8xFvSZtphPo8Hoj0ebHPs0eEwbF6Z9wv4gIe7aSghzpwVNIaXvb8Nc5zTv9OVopyJNwIbjGTzt00nY2rovSz"
     @cart = Cart.find(session[:cart_id])
+    subtotal = @cart.subtotal
+    line_items = []
     @cart.orderables.each do |i|
-      puts @product = Product.find_by(id: i.product_id)
+      line_items << {
+        price_data: {
+          currency:     "cad",
+          unit_amount:  (i.product.Price * 100).to_i,
+          product_data: {
+            name:        i.product.Name,
+            description: i.product.Description.empty? ? @product.Description : "No Description Avaliable"
+          },
+          tax_behavior: "exclusive"
+        },
+        quantity:   i.quantity
+      }
     end
+
+    line_items << {
+      price_data: {
+        currency:     "cad",
+        unit_amount:  (subtotal * 100 * current_user.province.tax.gst).to_i,
+        product_data: {
+          name:        "gst",
+          description: "Goods and Services Tax"
+        },
+        tax_behavior: "exclusive"
+      },
+      quantity:   1
+    }
+    line_items << {
+      price_data: {
+        currency:     "cad",
+        unit_amount:  (subtotal * 100 * current_user.province.tax.pst).to_i,
+        product_data: {
+          name:        "Pst",
+          description: "Provincal Sales Tax"
+        },
+        tax_behavior: "exclusive"
+      },
+      quantity:   1
+    }
 
     subtotal = @cart.subtotal
 
@@ -13,45 +51,8 @@ class CheckoutController < ApplicationController
       payment_method_types: ["card"],
       success_url:          checkout_success_url,
       cancel_url:           checkout_cancel_url,
-      line_items:           [
-        {
-          price_data: {
-            currency:     "cad",
-            unit_amount:  (@product.Price * 100).to_i,
-            product_data: {
-              name:        @product.Name,
-              description: @product.Description.empty? ? @product.Description : "No Description"
-            },
-            tax_behavior: "exclusive"
-          },
-          quantity:   1
-        },
-        {
-          price_data: {
-            currency:     "cad",
-            unit_amount:  (subtotal * 100 * current_user.province.tax.gst).to_i,
-            product_data: {
-              name:        "gst",
-              description: "Goods and Services Tax"
-            },
-            tax_behavior: "exclusive"
-          },
-          quantity:   1
-        },
-        {
-          price_data: {
-            currency:     "cad",
-            unit_amount:  (subtotal * 100 * current_user.province.tax.pst).to_i,
-            product_data: {
-              name:        "Pst",
-              description: "Provincal Sales Tax"
-            },
-            tax_behavior: "exclusive"
-          },
-          quantity:   1
-        }
+      line_items:           line_items,
 
-      ],
       mode:                 "payment"
     )
     redirect_to @session.url, allow_other_host: true
@@ -66,5 +67,6 @@ class CheckoutController < ApplicationController
 
   def cancel
     # something went wrong with payment!
+    redirect_to root_path
   end
 end
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,7 @@
 Rails.configuration.stripe = {
-  publishable_key: "pk_test_51M71cXAa7W4k6Y8xGkqfDVjHiNn528A1iYhbqfY2s5nnRZMYEHI8tOpogOi8UKKqUfM6xqXalILs1Uon0J5LH71D00PyHIYOi8"
-  secret_key: "sk_test_51M71cXAa7W4k6Y8xFvSZtphPo8Hoj0ebHPs0eEwbF6Z9wv4gIe7aSghzpwVNIaXvb8Nc5zTv9OVopyJNwIbjGTzt00nY2rovSz"
+  publishable_key: "pk_test_51M71cXAa7W4k6Y8xGkqfDVjHiNn528A1iYhbqfY2s5nnRZMYEHI8tOpogOi8UKKqUfM6xqXalILs1Uon0J5LH71D00PyHIYOi8",
+  secret_key:      "sk_test_51M71cXAa7W4k6Y8xFvSZtphPo8Hoj0ebHPs0eEwbF6Z9wv4gIe7aSghzpwVNIaXvb8Nc5zTv9OVopyJNwIbjGTzt00nY2rovSz"
 }
 
-#Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
 Stripe.api_key = "sk_test_51M71cXAa7W4k6Y8xFvSZtphPo8Hoj0ebHPs0eEwbF6Z9wv4gIe7aSghzpwVNIaXvb8Nc5zTv9OVopyJNwIbjGTzt00nY2rovSz"
