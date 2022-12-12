444609e8309293e3a7426c245f5f23e38179322c
Damon <DamonBaker27@hotmail.com>
2022-12-12 15:26:14 -0600

    updated migrations folder

--- db/migrate/20221107162135_create_orders.rb
+++ db/migrate/20221107162135_create_orders.rb
@@ -1,8 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-
       t.timestamps
+    end
+
+    create_table :order_items do |t|
+      t.timestamps
+      t.integer :quantity
     end
   end
 end
--- db/migrate/20221107173132_addcolumntotable.rb
+++ db/migrate/20221107173132_addcolumntotable.rb
@@ -1,6 +1,3 @@
 class Addcolumntotable < ActiveRecord::Migration[7.0]
-  def change
-    add_column :order_items, :quantity, :integer
-    add_column :order_items, :price, :integer
-  end
+  def change; end
 end
