6140adc5706aa10fed47331e9406e7d87804cc91
Damon <DamonBaker27@hotmail.com>
2022-12-12 14:55:40 -0600

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :set_render_cart
   before_action :initialize_cart
 
   protect_from_forgery with: :exception
@@ -7,12 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
-
-  def set_render_cart
-    @render_cart = true
-    # empty array of product ids
-    # session[:shopping_cart] ||= []
-  end
 
   def initialize_cart
     @cart ||= Cart.find_by(id: session[:cart_id])
